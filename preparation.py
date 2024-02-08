ca2a0fa04ffafd6d39391a6e20f20849bc4df513
Dmitry Bondar <bondar-dmitri@bk.ru>
2024-02-08 23:18:35 +1000

    fix bug

--- preparation.py
+++ preparation.py
@@ -2,10 +2,10 @@ import socket
 import os
 
 
-with open(os.path.expanduser('~/.bashrc'), 'a') as bash_file:
+with open(os.path.expanduser('./.env'), 'w+') as env_file:
     connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     connection.connect(('www.google.com', 443))
-    ip, port = connection.getsockname()
+    ip, _ = connection.getsockname()
     
-    bash_file.write(f'export IP={ip}\n')
+    env_file.write(f'IP={ip}\n')
     connection.close()
